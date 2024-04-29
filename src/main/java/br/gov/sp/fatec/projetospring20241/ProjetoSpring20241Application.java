8a3750cb7273e29ddeeb67ae99430e47ef6f8ecf
William Claudimar <will.2819@hotmail.com>
2024-04-29 19:54:33 -0300

    SPRINGBOOT

---
+++ src/main/java/br/gov/sp/fatec/projetospring20241/ProjetoSpring20241Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetospring20241;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring20241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring20241Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/projetospring20241/ProjetoSpring20241ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.projetospring20241;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring20241ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
